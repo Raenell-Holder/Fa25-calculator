ae814ac49ffcf0439945f134eeafeeb14cee4a26
Raenell Holder <157413349+Raenell-Holder@users.noreply.github.com>
2025-09-09 20:09:45 +0000

    merged order

--- Calculator.java
+++ Calculator.java
@@ -20,20 +20,19 @@ public class Calculator
         Scanner operators = new Scanner(System.in); 
         Scanner operands = new Scanner(System.in); 
     } 
-
-    public int addition(int x, int y) 
-    { 
-       //return x + y; 
+   public int multiplication(int x, int y) 
+    {     
+       //return x * y; 
     } 
     public int subtraction(int x, int y) 
     { 
        //return x - y; 
     } 
-    
-    public int multiplication(int x, int y) 
-    {     
-       //return x * y; 
+    public int addition(int x, int y) 
+    { 
+       //return x + y; 
     } 
+  
     public int division(int x, int y) 
     { 
        //solution = x / y; 
